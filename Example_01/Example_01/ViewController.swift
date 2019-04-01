ad88268f8c5b1931aa9407bd3737f2c0bb108486
HaoLe <thienhaole92@gmail.com>
2019-04-01 07:08:36 -0700

    no message

--- Example_01/Example_01/ViewController.swift
+++ Example_01/Example_01/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var inputTextField2: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func actionButtonTapped(_ sender: Any) {
+        let inputText: String? = inputTextField.text
+        if inputText != nil && inputText!.count > 0 {
+            print(inputText!.count)
+            textLabel.text = "Gia tri ban nhap vao la: \(inputText!)"
+            inputTextField.text = nil
+        } else {
+            textLabel.text = "Khong nhap gi ca"
+        }
+    }
+    
+    @IBAction func sumButtonTapped(_ sender: Any) {
+        let firstNum = Double(inputTextField.text!)
+        let secondNum = Double(inputTextField2.text!)
+        
+        if firstNum == nil && secondNum == nil {
+            textLabel.text = "Ban nhap sai ca hai so"
+            return
+        }
+        
+        if firstNum == nil {
+            textLabel.text = "Ban nhap vao so thu nhat: \(inputTextField.text!) - Sai roi"
+            return
+        }
+        
+        if secondNum == nil {
+            textLabel.text = "Ban nhap vao so thu hai: \(inputTextField2.text!) - Sai roi"
+            return
+        }
+        
+        if firstNum != nil && secondNum != nil {
+            let sum = firstNum! + secondNum!
+            textLabel.text = "Ket qua la: \(sum)"
+        } else {
+            print("Co mot so bang nil")
+        }
 
+    }
 
 }
 
