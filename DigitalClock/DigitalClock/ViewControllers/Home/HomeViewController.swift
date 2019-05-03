0a58be8159e0201dbdeb309979408b3afae203ff
HaoLe <thienhaole92@gmail.com>
2019-05-03 21:15:25 +0700

    no message

--- DigitalClock/DigitalClock/ViewControllers/Home/HomeViewController.swift
+++ DigitalClock/DigitalClock/ViewControllers/Home/HomeViewController.swift
@@ -10,20 +10,16 @@ import UIKit
 import SnapKit
 
 class HomeViewController: UIViewController {
-    
+
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var weekDateLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
-    
+
     var timerButton: UIButton!
     var settingButton: UIButton!
     var themeButton: UIButton!
-    var notiButton:  UIButton!
-    
-    var didSetupConstraints = false
-    var timer: Timer!
-    
+    var notiButton: UIButton!
     var notiStatusLabel: UILabel = {
         let lb = UILabel()
         lb.text = "OFF"
@@ -31,7 +27,10 @@ class HomeViewController: UIViewController {
         lb.translatesAutoresizingMaskIntoConstraints = false
         return lb
     }()
-    
+
+    var didSetupConstraints = false
+    var timer: Timer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         timer = Timer.scheduledTimer(
@@ -42,7 +41,7 @@ class HomeViewController: UIViewController {
             repeats: true)
         setUpUI()
     }
-    
+
     func setUpUI() {
         //set up timer button
         timerButton = UIButton()
@@ -50,32 +49,32 @@ class HomeViewController: UIViewController {
         timerButton.setImage(UIImage(named: "ic_timer"), for: .normal)
         timerButton.addTarget(self, action: #selector(self.timerButtonClick(_:)), for: .touchUpInside)
         view.addSubview(timerButton)
-        
+
         //set up setting button
         settingButton = UIButton()
         settingButton.translatesAutoresizingMaskIntoConstraints = false
         settingButton.setImage(UIImage.init(named: "ic_settings"), for: .normal)
         settingButton.addTarget(self, action: #selector(self.settingButtonClick(_:)), for: .touchUpInside)
         view.addSubview(settingButton)
-        
+
         //set up theme Button
         themeButton = UIButton()
         themeButton.translatesAutoresizingMaskIntoConstraints = false
         themeButton.setImage(UIImage(named: "ic_themes"), for: .normal)
         themeButton.addTarget(self, action: #selector(self.themeButtonClick(_:)), for: .touchUpInside)
         view.addSubview(themeButton)
-        
+
         //set up noti Button
         notiButton = UIButton()
         notiButton.translatesAutoresizingMaskIntoConstraints = false
         notiButton.setImage(UIImage(named: "ic_notification"), for: .normal)
         notiButton.addTarget(self, action: #selector(self.notiButtonClick(_:)), for: .touchUpInside)
         view.addSubview(notiButton)
-        
+
         view.addSubview(notiStatusLabel)
     }
-    
-    
+
+
     override func updateViewConstraints() {
         if (!didSetupConstraints) {
             timerButton.snp.makeConstraints { (make) in
@@ -83,39 +82,39 @@ class HomeViewController: UIViewController {
                 make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(25.0)
                 make.width.height.equalTo(25.0)
             }
-            
+
             settingButton.snp.makeConstraints { (make) in
                 make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(5.0)
                 make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(25.0)
                 make.width.height.equalTo(25.0)
             }
-            
+
             themeButton.snp.makeConstraints { (make) in
                 make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(5.0)
                 make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(25.0)
                 make.width.height.equalTo(25.0)
             }
-            
+
             notiButton.snp.makeConstraints { (make) in
                 make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(5.0)
                 make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).inset(25.0)
                 make.width.height.equalTo(25.0)
             }
-            
+
             notiStatusLabel.snp.makeConstraints { (make) in
                 make.centerY.equalTo(notiButton)
                 make.leading.equalTo(notiButton.snp.trailing).offset(10.0)
             }
-            
+
             didSetupConstraints = true
         }
         super.updateViewConstraints()
     }
-    
+
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.hourLabel.text = nil
@@ -123,7 +122,7 @@ class HomeViewController: UIViewController {
         self.weekDateLabel.text = nil
         self.secondLabel.text = nil
     }
-    
+
     func getCurrentHourString() -> String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -131,7 +130,7 @@ class HomeViewController: UIViewController {
         let currentDateString = dateFormatter.string(from: date)
         return currentDateString
     }
-    
+
     func getCurrentDateString() -> String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -139,7 +138,7 @@ class HomeViewController: UIViewController {
         let currentDateString = dateFormatter.string(from: date)
         return currentDateString
     }
-    
+
     func getCurrentWeekDateString() -> String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -147,7 +146,7 @@ class HomeViewController: UIViewController {
         let currentDateString = dateFormatter.string(from: date)
         return currentDateString
     }
-    
+
     func getCurrentSecondString() -> String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -155,7 +154,7 @@ class HomeViewController: UIViewController {
         let currentDateString = dateFormatter.string(from: date)
         return currentDateString
     }
-    
+
     @objc func fire() {
         let hourString = getCurrentHourString()
         let dateString = getCurrentDateString()
@@ -170,19 +169,19 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController {
-    
+
     @IBAction func timerButtonClick(_ sender: Any) {
-        
+
     }
-    
+
     @IBAction func settingButtonClick(_ sender: Any) {
         print("Setting Button")
     }
-    
+
     @IBAction func themeButtonClick(_ sender: Any) {
-        
+
     }
     @IBAction func notiButtonClick(_ sender: Any) {
-        
+
     }
 }
